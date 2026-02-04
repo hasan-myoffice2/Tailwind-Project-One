e7c003536538a1ce77da8b6d17acd12cb57d3674
Hasan <hasan.myoffice2@gmail.com>
2026-02-04 17:35:10 +0600

    Project Complate

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const sidebar = document.getElementById("mobile-sidebar");
+  sidebar.classList.toggle("translate-x-full");
+}
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
